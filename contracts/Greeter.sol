fc03d37f0940b238503c9306a9945159b6e7f0c9
OceanT-shirt <stylish.trousers@gmail.com>
2022-04-17 10:56:29 +0900

    Add: openzeppelin

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,20 +1,10 @@
 pragma solidity ^0.8.0;
 
-contract Greeter {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Greeter is Ownable {
     string private _greeting = "Hello, World!";
-    address private _owner;
 
-    constructor() public {
-        _owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(
-        msg.sender == _owner,
-        "Ownable: caller is not the owner"
-        );
-        _;
-    }
 
     function greet() external view returns(string memory){
         return _greeting;
@@ -25,7 +15,4 @@ contract Greeter {
         _greeting = greeting;
     }
 
-    function owner() public view returns(address) {
-        return _owner;
-    }
 }
